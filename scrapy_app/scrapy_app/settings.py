59d3cba9ba5e49d11292896fce4d16a2dc75cb7c
Abhishek <sandeepgoswami108@gmail.com>
2018-09-17 19:43:56 +0530

    sources

--- scrapy_app/scrapy_app/settings.py
+++ scrapy_app/scrapy_app/settings.py
@@ -227,9 +227,7 @@ USER_AGENT_LIST = [
 
 ]
 
-URL_FILES = [
-    dir_path + '/urls_all.txt',
-]
+URL_FILES = ['https://hunter.io/companies/letters/0/1/','https://hunter.io/companies/letters/1/1/','https://hunter.io/companies/letters/2/1/','https://hunter.io/companies/letters/3/1/','https://hunter.io/companies/letters/4/1/','https://hunter.io/companies/letters/5/1/','https://hunter.io/companies/letters/6/1/','https://hunter.io/companies/letters/7/1/','https://hunter.io/companies/letters/8/1/','https://hunter.io/companies/letters/9/1/','https://hunter.io/companies/letters/a/1/','https://hunter.io/companies/letters/b/1/','https://hunter.io/companies/letters/c/1/','https://hunter.io/companies/letters/d/1/','https://hunter.io/companies/letters/e/1/','https://hunter.io/companies/letters/f/1/','https://hunter.io/companies/letters/g/1/','https://hunter.io/companies/letters/h/1/','https://hunter.io/companies/letters/i/1/','https://hunter.io/companies/letters/j/1/','https://hunter.io/companies/letters/k/1/','https://hunter.io/companies/letters/l/1/','https://hunter.io/companies/letters/m/1/','https://hunter.io/companies/letters/n/1/','https://hunter.io/companies/letters/o/1/','https://hunter.io/companies/letters/p/1/','https://hunter.io/companies/letters/q/1/','https://hunter.io/companies/letters/r/1/','https://hunter.io/companies/letters/s/1/','https://hunter.io/companies/letters/t/1/','https://hunter.io/companies/letters/u/1/','https://hunter.io/companies/letters/v/1/','https://hunter.io/companies/letters/w/1/','https://hunter.io/companies/letters/x/1/','https://hunter.io/companies/letters/y/1/','https://hunter.io/companies/letters/z/1/']
 # set to none to get all links in a page
 COMPANY_LIMIT = os.getenv('COMPANY_LIMIT', None)
 # set to none to get all source links
@@ -241,3 +239,10 @@ if SOURCE_LIMIT:
     SOURCE_LIMIT = int(SOURCE_LIMIT)
 if MONGODB_PORT:
     MONGODB_PORT = int(MONGODB_PORT)
+
+
+
+
+
+
+
