6bf62c33dc3c137e12df86a1d0c740452b0a8d3f
root <sandeepgoswami108@gmail.com>
2018-10-02 13:33:07 +0000

    changes in server

--- main2/admin.py
+++ main2/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import *
+class CompanyItemNewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'employees')
+    search_fields = ('name', 'url')
+    list_per_page = 50
 
-admin.site.register(CompanyItemNew)
-admin.site.register(HunterItemNew)
-admin.site.register(SourceItemNew)
+admin.site.register(CompanyItemNew, CompanyItemNewAdmin)
+
+class HunterItemNewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'pattern')
+    search_fields = ('name', 'email')
+    list_per_page = 50
+
+admin.site.register(HunterItemNew, HunterItemNewAdmin)
+#admin.site.register(CompanyItemNew)
+#admin.site.register(HunterItemNew)
+#admin.site.register(SourceItemNew)
 
--- scrapy_app/scrapy_app/spiders/hunter.py
+++ scrapy_app/scrapy_app/spiders/hunter.py
@@ -22,10 +22,19 @@ class Hunter(scrapy.Spider):
 
 
     name = 'hunter'
-    def start_requests(self):
-        urls = ['https://hunter.io/companies/letters/0/1/','https://hunter.io/companies/letters/1/1/','https://hunter.io/companies/letters/2/1/','https://hunter.io/companies/letters/3/1/','https://hunter.io/companies/letters/4/1/','https://hunter.io/companies/letters/5/1/','https://hunter.io/companies/letters/6/1/','https://hunter.io/companies/letters/7/1/','https://hunter.io/companies/letters/8/1/','https://hunter.io/companies/letters/9/1/','https://hunter.io/companies/letters/a/1/','https://hunter.io/companies/letters/b/1/','https://hunter.io/companies/letters/c/1/','https://hunter.io/companies/letters/d/1/','https://hunter.io/companies/letters/e/1/','https://hunter.io/companies/letters/f/1/','https://hunter.io/companies/letters/g/1/','https://hunter.io/companies/letters/h/1/','https://hunter.io/companies/letters/i/1/','https://hunter.io/companies/letters/j/1/','https://hunter.io/companies/letters/k/1/','https://hunter.io/companies/letters/l/1/','https://hunter.io/companies/letters/m/1/','https://hunter.io/companies/letters/n/1/','https://hunter.io/companies/letters/o/1/','https://hunter.io/companies/letters/p/1/','https://hunter.io/companies/letters/q/1/','https://hunter.io/companies/letters/r/1/','https://hunter.io/companies/letters/s/1/','https://hunter.io/companies/letters/t/1/','https://hunter.io/companies/letters/u/1/','https://hunter.io/companies/letters/v/1/','https://hunter.io/companies/letters/w/1/','https://hunter.io/companies/letters/x/1/','https://hunter.io/companies/letters/y/1/','https://hunter.io/companies/letters/z/1/']
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    #def start_requests(self):
+        #urls = ['https://hunter.io/companies/letters/0/1/','https://hunter.io/companies/letters/1/1/','https://hunter.io/companies/letters/2/1/','https://hunter.io/companies/letters/3/1/','https://hunter.io/companies/letters/4/1/','https://hunter.io/companies/letters/5/1/','https://hunter.io/companies/letters/6/1/','https://hunter.io/companies/letters/7/1/','https://hunter.io/companies/letters/8/1/','https://hunter.io/companies/letters/9/1/','https://hunter.io/companies/letters/a/1/','https://hunter.io/companies/letters/b/1/','https://hunter.io/companies/letters/c/1/','https://hunter.io/companies/letters/d/1/','https://hunter.io/companies/letters/e/1/','https://hunter.io/companies/letters/f/1/','https://hunter.io/companies/letters/g/1/','https://hunter.io/companies/letters/h/1/','https://hunter.io/companies/letters/i/1/','https://hunter.io/companies/letters/j/1/','https://hunter.io/companies/letters/k/1/','https://hunter.io/companies/letters/l/1/','https://hunter.io/companies/letters/m/1/','https://hunter.io/companies/letters/n/1/','https://hunter.io/companies/letters/o/1/','https://hunter.io/companies/letters/p/1/','https://hunter.io/companies/letters/q/1/','https://hunter.io/companies/letters/r/1/','https://hunter.io/companies/letters/s/1/','https://hunter.io/companies/letters/t/1/','https://hunter.io/companies/letters/u/1/','https://hunter.io/companies/letters/v/1/','https://hunter.io/companies/letters/w/1/','https://hunter.io/companies/letters/x/1/','https://hunter.io/companies/letters/y/1/','https://hunter.io/companies/letters/z/1/']
+        #for url in urls:
+            #print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            #print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            #print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            #print(url)
+            #yield scrapy.Request(url=url, callback=self.parse)
+    def __init__(self, urls, **kwargs):
+        self.proxy_pool = PROXIES
+        #self.USER_AGENT_LIST = USER_AGENT_LIST
+        self.start_urls = urls
+        super(Hunter, self).__init__(**kwargs)
     def parse(self, response):
         no_of_pages = Selector(response).xpath('/html/body/section/div/div/div/a/text()').extract()[-1]
         try:
@@ -107,12 +116,6 @@ class Hunter(scrapy.Spider):
                     #print(query)
                     cur.execute(query)
                     conn.commit()
-                    print('******************')
-                    print('******************')
-                    print('******************')
-                    print('******************')
-                    print('******************')
-                    print('******************')
 
                     #cur.execute("insert into main2_hunteritemnew(name,email,pattern,sources,html) values('"+item['name']+ "', '"+item['email']+"','"+item['pattern']+"','"+item['sources']+"','"+item['html']+"',);")
                 cur.close()
@@ -135,9 +138,6 @@ class Hunter(scrapy.Spider):
                 conn.commit()
             cur.close()
             conn.close()
-            print('===========================')
-            print('===========================')
-            print('===========================')
             yield company
         except AttributeError as e:
             print(str(e.args))
